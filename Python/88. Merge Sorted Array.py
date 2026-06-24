200b97265c7229da1d6661c38039bdad53087f0c
Uday Awasthi <officialuday2007@gmail.com>
2026-06-24 08:35:06 +0530

    Add topics to the Merge Sorted Array problem
    Added topics for better categorization.

--- Python/88. Merge Sorted Array.py
+++ Python/88. Merge Sorted Array.py
@@ -1,5 +1,6 @@
 Ques - Leetcode - 88. Merge Sorted Array
 
+Topics - Array , Two Pointers, Sorting
 # You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
 # representing the number of elements in nums1 and nums2 respectively.
 
