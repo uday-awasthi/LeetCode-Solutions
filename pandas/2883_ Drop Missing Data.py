8c149c3f69cb18a639fe6c284384cacf47b7d8e0
Uday Awasthi <officialuday2007@gmail.com>
2026-06-24 08:05:31 +0530

    Create 2883_ Drop Missing Data.py

---
+++ pandas/2883_ Drop Missing Data.py
@@ -0,0 +1,46 @@
+Ques - 2883. Drop Missing Data
+
+There are some rows having missing values in the name column.
+
+Write a solution to remove the rows with missing values.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input:
++------------+---------+-----+
+| student_id | name    | age |
++------------+---------+-----+
+| 32         | Piper   | 5   |
+| 217        | None    | 19  |
+| 779        | Georgia | 20  |
+| 849        | Willow  | 14  |
++------------+---------+-----+
+Output:
++------------+---------+-----+
+| student_id | name    | age |
++------------+---------+-----+
+| 32         | Piper   | 5   |
+| 779        | Georgia | 20  | 
+| 849        | Willow  | 14  | 
++------------+---------+-----+
+Explanation: 
+Student with id 217 havs empty value in the name column, so it will be removed.
+
+
+
+## SOLUTION 
+  import pandas as pd
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.dropna(subset=["name"])
+
+
+
+
+
+
+  
