88feabd09590b7f1ca7338e9f3db55993c3522ad
Uday Awasthi <officialuday2007@gmail.com>
2026-06-24 08:34:11 +0530

    Add merge function for merging two sorted arrays
    Implement merge function to combine two sorted arrays into nums1.

---
+++ Python/88. Merge Sorted Array.py
@@ -0,0 +1,32 @@
+Ques - Leetcode - 88. Merge Sorted Array
+
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
+# representing the number of elements in nums1 and nums2 respectively.
+
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+# The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+# To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
+# and the last n elements are set to 0 and should be ignored.nums2 has a length of n.
+
+## SOLUTION 
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m - 1 # nums1 ka last valid element
+        j = n - 1  # nums2 ka last element
+        k = m + n - 1  # nums1 ki last position
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
