712cc785b5f1ac86bfe55ab2b386e26deb52c4ab
Uday Awasthi <officialuday2007@gmail.com>
2026-06-26 18:34:48 +0530

    Add searchInsert function for target index lookup
    Implement binary search algorithm to find the target index or insertion point in a sorted array.

---
+++ Python/35. Search Insert Position.py
@@ -0,0 +1,21 @@
+# DESCRIPTION :
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ ## SOLUTION 
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else :
+                right = mid - 1
+        return left
