284b621356f473a541f2e920035ee5b3cb1d19b4
Uday Awasthi <officialuday2007@gmail.com>
2026-06-29 11:30:58 +0530

    Create 183. Customers Who Never Order.py

---
+++ pandas/183. Customers Who Never Order.py
@@ -0,0 +1,78 @@
+Ques - 183. Customers Who Never Order
+
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+customerId is a foreign key (reference columns) of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+Write a solution to find all customers who never order anything.
+
+Return the result table in any order.
+
+
+
+## SOLUTION 
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    df = pd.merge(
+        customers,
+        orders,
+        how='left',
+        left_on='id',
+        right_on='customerId'
+    )
+
+    df = df[df['customerId'].isna()]
+
+    return df[['name']].rename(columns={'name': 'Customers'}) 
+
+## EXAMPLE
+
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
