136460a441dd6e8007b5196f80bdc1d0fe534731
Uday Awasthi <officialuday2007@gmail.com>
2026-07-22 22:01:49 +0530

    Add function to identify invalid tweets
    Implement function to filter invalid tweets based on character count.

---
+++ pandas/1683. Invalid Tweets.py
@@ -0,0 +1,25 @@
+
+Table: Tweets
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+content consists of alphanumeric characters, '!', or ' ' and no other special characters.
+This table contains all the tweets in a social media app.
+ 
+
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+
+
+## SOlUTION
+
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    return tweets[tweets["content"].str.len() > 15][["tweet_id"]]
