48db51644d1358f4eb1c0274a5b0f91990823114
Uday Awasthi <officialuday2007@gmail.com>
2026-07-30 00:57:48 +0530

    Create 412. Fizz Buzz.py

---
+++ Python/412. Fizz Buzz.py
@@ -0,0 +1,33 @@
+Ques - 412. Fizz Buzz
+## Description 
+
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+
+
+ ## SOLUTION 
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = []
+        for i in range (1,n+1):
+            if i %3==0 and i % 5 == 0:
+                ans.append("FizzBuzz")
+            elif i %3 ==0:
+                ans.append("Fizz")
+            elif i % 5 ==0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+        return ans
+        
