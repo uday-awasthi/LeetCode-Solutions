2ac4177c5faabaf3b85eadaebdc4d9f7f5529839
Uday Awasthi <officialuday2007@gmail.com>
2026-07-30 21:47:59 +0530

    Update and rename QUES - 281. Subtract the Product and Sum of Digits of an Integer.py to QUES - 1281. Subtract the Product and Sum of Digits of an Integer.py

--- Python/QUES - 1281. Subtract the Product and Sum of Digits of an Integer.py
+++ Python/QUES - 1281. Subtract the Product and Sum of Digits of an Integer.py
@@ -1,4 +1,4 @@
-QUES - 281. Subtract the Product and Sum of Digits of an Integer
+QUES - 1281. Subtract the Product and Sum of Digits of an Integer
 
 Given an integer number n, return the difference between the product of its digits and the sum of its digits.
 
