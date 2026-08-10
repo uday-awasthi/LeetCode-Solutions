1df75fca7cf3bf6d518de70ef7fc6322bc1f07ba
Uday Awasthi <officialuday2007@gmail.com>
2026-08-10 22:39:29 +0530

    Time: 344 ms (11.37%), Space: 67.3 MB (60.44%) - LeetHub

---
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    pattern = r'^[A-Za-z][A-Za-z0-9_.-]*@leetcode\.com$'
+    
+    return users[users['mail'].str.match(pattern)]
