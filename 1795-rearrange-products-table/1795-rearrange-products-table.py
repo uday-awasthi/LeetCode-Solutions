014941631a075d930f83f110226cc9ad6fc26a05
Uday Awasthi <officialuday2007@gmail.com>
2026-09-01 23:02:42 +0530

    Time: 372 ms (14.76%), Space: 67.1 MB (85.08%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    products = products.melt(
+        id_vars=["product_id"],
+        var_name="store",
+        value_name="price"
+    )
+
+    products = products.dropna()
+
+    return products
